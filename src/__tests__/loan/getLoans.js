6f2dee51d630386a6a1775234b8828accb9afb5b
Olalekan Animashaun <kim.olalekan@gmail.com>
2021-03-11 11:18:54 +0100

    Initial commit

--- src/__tests__/loan/getLoans.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("../../server");
-describe("Get loans", () => {
-  test("should list loans", async (done) => {
-    const res = await request(app).get("/api/loans?page=1&limit=50");
-    expect(res.statusCode).toEqual(200);
-    done();
-  });
-});
--- src/__tests__/wallet/getWallet.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("../../server");
-describe("Get single wallet", () => {
-  test("should get a wallet", async (done) => {
-    const res = await request(app).get("/api/wallet/C45D-0804");
-    expect(res.statusCode).toEqual(200);
-    done();
-  }, 30000);
-});
